aa1e3f02d12537c49ea72148245414300cfff9a3
jan <jan.palucki@gmail.com>
2018-11-20 01:07:55 +0100

    Multithreaded Monte Carlo implementation with separate random method

--- lab3/lab3.cpp
+++ lab3/lab3.cpp
@@ -8,10 +8,11 @@ using namespace std;
 
 double fun(double x);
 double doubleRand();
+double doubleRandForNthThread(unsigned int n);
 
 int main(){
 
-	double dx=0.0000001; 
+	double dx=0.00000001; 
 	int p=1/dx;
 	double res[4];
 	double sin_res=0;
@@ -25,7 +26,7 @@ int main(){
 	double single_end_time = omp_get_wtime();
 
 	cout << "Single thread result x4 =" << sin_res*4 << "\n";
-	cout << "Single thread execution took " << single_end_time - single_start_time << "[s]\n";
+	cout << "Single thread execution took " << single_end_time - single_start_time << "[s]\n\n";
 	
 	double multi_start_time = omp_get_wtime();
 
@@ -54,7 +55,7 @@ int main(){
 	double multi_end_time = omp_get_wtime();
 
 	cout << "Total result x4 is: " << total*4 << "\n";
-	cout << "Multi thread execution took " << multi_end_time - multi_start_time << "[s]\n";
+	cout << "Multi thread execution took " << multi_end_time - multi_start_time << "[s]\n\n";
 
 	// PRAGMA REDUCTION SYNTAX
 
@@ -64,12 +65,13 @@ int main(){
 		red_res=red_res+fun(l*dx)*dx;
 	}
 	
-	cout << "Reduction_result=" << red_res*4 << "\n";
+	cout << "Reduction_result=" << red_res*4 << "\n\n";
 
 	srand(static_cast<unsigned int>(clock()));
-	double pts_num = 1000000;
+	double pts_num = 10000000;
 	double inside_pts_num = 0;
 	double r=0.5;
+	double carlo_start_time = omp_get_wtime();
 
 	for(int i=0;i<pts_num;i++){
 		double x=doubleRand();
@@ -86,12 +88,44 @@ int main(){
     // tez pi/4. A wiec aby uzyskac przyblizenie liczby pi wystarczy
     // policzyc stosunek punktow_w_kole do punktow_w_kwadracie razy 4
 	
+	double carlo_end_time = omp_get_wtime();
 	double circle_area = inside_pts_num/pts_num; //statistically calculated circe area
 	double pi = circle_area * 4; //because circle area is pi*r^2
 	//cout << "Inside points: " << inside_pts_num << "\n";
 	//cout << "circle_area = " << circle_area << "\n"; 
-	cout << "Circle points in/all x4 = " << pi << "\n"; 
+	cout << "Circle points in/all x4 = " << pi << "\n";
+	cout << "Circle points in/all took " << carlo_end_time-carlo_start_time << "[s]\n\n"; 
 
+
+	double multi_insides[4];
+	double circle_total=0;
+	double carlo_multi_start_time = omp_get_wtime();
+	#pragma omp parallel
+	{
+		unsigned int k=omp_get_thread_num();
+		unsigned int n=omp_get_num_threads();
+		double thread_res=0;
+
+		for(int i=k;i<pts_num;i+=4){
+			double x=doubleRandForNthThread(k);
+			double y=doubleRandForNthThread(k);
+			double distance = sqrt(pow(r-x,2)+pow(r-y,2));
+
+			if(distance < r){
+				thread_res++;
+			}
+		}
+
+		multi_insides[k]=thread_res;
+	}
+
+	for(int j=0;j<4;j++){
+		circle_total+=multi_insides[j];
+	}
+
+	double carlo_multi_end_time = omp_get_wtime();
+	cout << "Multithreaded: Circle points in/all x4 = " << (circle_total/pts_num)*4 << "\n"; 
+	cout << "Multithreaded: Circle points in/all took " << carlo_multi_end_time-carlo_multi_start_time << "[s]\n\n"; 
 
 	return 0;
 }
@@ -102,6 +136,10 @@ double fun(double x){
 
 double doubleRand() {
   return double(rand()) / (double(RAND_MAX) + 1.0);
+}
+
+double doubleRandForNthThread(unsigned int n) {
+  return double(rand_r(&n)) / (double(RAND_MAX) + 1.0);
 }
 
 
